f250a6edab136153a726ab1dedcabbdad763a13a
Insoo-PK <insoodever@gmail.com>
2024-02-13 22:03:11 +0900

    가변인수 추가

--- Java-Practice/src/main/java/com/insoo/javapractice/etc/Generic.java
+++ Java-Practice/src/main/java/com/insoo/javapractice/etc/Generic.java
@@ -1,25 +1,26 @@
 package com.insoo.javapractice.etc;
 
 /**
-    T => Type
-    K => Key
-    V => Value
-    E => Element(자료구조에 들어가는 원소들을 나타낼 때 사용함)
+    T : Type
+    K : Key
+    V : Value
+    E : Element(자료구조에 들어가는 원소들을 나타낼 때 사용함)
  */
 public class Generic {
     public static void main(String[] args){
 
-        // 제네릭 클래스 사용
         Box<Integer> integerBox = new Box<>();
         integerBox.setValue(10);
         System.out.println("Box value: " + integerBox.getValue());
-
-        // 제네릭 메서드 사용
         Box.printValue("Hello, Generics!");
 
+        double sumResult = doubleSum(3.24, 3);
+        System.out.println("#### doubleSum : " + sumResult);
     }
 
-    // 제네릭 클래스
+    /**
+     * 제네릭 클래스
+     */
     public static class Box<T> {
         private T value;
 
@@ -31,10 +32,24 @@ public class Generic {
             this.value = value;
         }
 
-        // 제네릭 메서드
+        /**
+         * 제네릭 메서드
+         */
         public static <T> void printValue(T value) {
             System.out.println("Value: " + value);
         }
     }
 
+    /**
+        제한된 타입 파라미터
+        : T 옆에 extends Number로 통해서 적어도 Number타입 또는 Number의 자식들만 오게 할 수 있다.
+     */
+    public static <T extends Number> Double doubleSum(T num1, T num2){
+        double v1 = num1.doubleValue();
+        double v2 = num1.doubleValue();
+
+        return Double.sum(v1, v2);
+    }
+
 }
+
